c002c2424f3dd0cfc7d914f7f13b5439cdc79108
diyorsattarov <diyorsattarov@outlook.com>
2024-11-02 19:56:38 -0500

    Added package.bar - javac -d out src/main.java src/foo/foo.java bar/bar.java
    - java -cp out main

--- src/main.java
+++ src/main.java
@@ -1,8 +1,10 @@
 import foo.foo;
+import bar.bar;
 
 class main {
     public static void main(String []args) {
         System.out.println("helloworld");
         foo.foo();
+        bar.bar();
     }
 }
