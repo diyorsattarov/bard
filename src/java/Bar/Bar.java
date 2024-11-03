cb3c3bb0076d13b721b50ab1dc473db39d40e02d
diyorsattarov <diyorsattarov@outlook.com>
2024-11-03 14:36:33 -0600

    Added comments

--- src/java/Bar/Bar.java
+++ src/java/Bar/Bar.java
@@ -9,28 +9,33 @@ public class Bar {
         barSystem();
         barThread();
     }
-
+    
+    // java.lang.Object
     private static void barObject() {
         Object qux = new Object();
         System.out.println("barObject hashcode: " + qux.hashCode());
     }
 
+    // java.lang.String
     private static void barString() {
         String qux = "bar and qux";
         System.out.println("barString length: " + qux.length());
     }
 
+    // java.lang.Math
     private static void barMath() {
         int foo = 5;
         int qux = 10;
         System.out.println("barMath max: " + Math.max(foo, qux));
     }
     
+    // java.lang.System
     private static void barSystem() {
         long quxT = System.currentTimeMillis();
         System.out.println("barSystem time: " + quxT);           
     }
-
+    
+    // java.lang.Thread
     private static void barThread() { 
         Thread qux = new Thread(() -> System.out.println("barThread running"));
         qux.start();
